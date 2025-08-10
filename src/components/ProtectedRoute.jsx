bdd8e40b223136600ee04c93ff6368623aba1798
Yash Midha <72779011+YashMidha@users.noreply.github.com>
2025-08-10 22:06:19 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "@/context/authContext.jsx";
+import { useAuth } from "@/context/AuthContext.jsx";
 import Loading from "./Loading";
 
 export default function ProtectedRoute({ children }) {
